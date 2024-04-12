fe9472b278b940213a6378bbaf248c07dcb9eae0
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-04-13 01:19:08 +0600

    Add count step problem

--- Module-5.5/distwnce.c
+++ Module-5.5/distwnce.c
@@ -1,9 +1,14 @@
-#include<stdio.h>
+#include <stdio.h>
 int main()
 {
     int a;
-    scanf("%d",&a);
-     
-    printf("%d",a);
+    scanf("%d", &a);
+    int step = a / 3;
+    int reminderStep = a % 3;
+    if (reminderStep == 1 || reminderStep == 2)
+    {
+        step = step + 1;
+    }
+    printf("%d", step);
     return 0;
 }
