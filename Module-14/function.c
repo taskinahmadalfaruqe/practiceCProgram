52c0789ebfba84c6ec5ca08f9c33df2673d44855
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-04-29 22:09:13 +0600

    Add function module

---
+++ Module-14/function.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<string.h>
+#include<limits.h>
+#include <stdio.h>
+#include <stdio.h>
+int sum(int x, int y)
+{
+    int res = x + y;
+    return res;
+}
+int main()
+{
+    sum(5, 6);
+    return 0;
+}
