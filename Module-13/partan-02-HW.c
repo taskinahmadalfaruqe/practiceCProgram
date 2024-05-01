1ad85d45761e33d52076271b0e89f8b65a729b85
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-05-01 22:31:26 +0600

    Add Partan-02 with Home Work

---
+++ Module-13/partan-02-HW.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+int main()
+{
+    int a;
+    scanf("%d", &a);
+    int s = 0, k = 2 * a - 1;
+    for (int i = 0; i < a; i++)
+    {
+        for (int b = 0; b < s; b++)
+        {
+            printf(" ");
+        }
+        for (int b = 0; b < k; b++)
+        {
+            printf("*");
+        }
+        s++;
+        k -= 2;
+        printf("\n");
+    }
+
+    return 0;
+}
