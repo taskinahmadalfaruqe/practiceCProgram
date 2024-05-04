5a9c27717e4277eb3a0e0ae81e23bdccaeb8e620
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-05-04 18:57:14 +0600

    Add Sumation Problem With Function

---
+++ Module-15.5/A_Add.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+int sum(int x, int y)
+{
+    int summation = x + y;
+    return summation;
+}
+int main()
+{
+    int a, b;
+    scanf("%d%d", &a, &b);
+    int summation = sum(a, b);
+    printf("%d", summation);
+
+    return 0;
+}
