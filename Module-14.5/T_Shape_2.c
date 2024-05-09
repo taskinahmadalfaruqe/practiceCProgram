a8c1492c5aeec2b8b12fe3621d2c1d3c9901ea27
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-05-09 20:00:25 +0600

    Add Mirror Array Problem

--- Module-14.5/T_Shape_2.c
+++ Module-14.5/T_Shape_2.c
@@ -8,7 +8,7 @@ int main()
     s = a;
     for (int i = 1; i <= a; i++)
     {
-        for (int j = 0; j < s; j++)
+        for (int j = 1; j < s; j++)
         {
             printf(" ");
         }
---
+++ Module-19/W_Mirror_Array.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <limits.h>
+int main()
+{
+    int row, col;
+    scanf("%d %d", &row, &col);
+    int ar[row][col];
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < col; j++)
+        {
+            scanf("%d", &ar[i][j]);
+        }
+    }
+
+    for (int a = 0; a < row; a++)
+    {
+        for (int i = col-1; i >= 0; i--)
+        {
+            printf("%d ", ar[a][i]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
