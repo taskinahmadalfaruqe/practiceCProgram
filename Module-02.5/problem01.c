3445e0ddb6f7388920e449fda2f1e45ab95b2e63
TASKIN AHMAD AL FARUQE <taskinahmed180@gmail.com>
2024-10-17 17:05:03 +0600

    Update problem01.c

--- Module-02.5/problem01.c
+++ Module-02.5/problem01.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 int main()
 {
+    int a,b;
+    scanf("%d%d",&a,&b);
+    if(a>b){
+        printf("A IS Geter Then B: %d:",a);
+    }else{
+        printf("B IS Geter Then A: %d:",b);
+    }
     return 0;
 }
