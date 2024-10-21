35318869c02787c561bab427b4daf0cabb6f7bf6
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2024-10-21 22:08:51 +0800

    Add Previous Problem Wit Full Salution

--- Module-02/Small_letter.c
+++ Module-02/Small_letter.c
@@ -5,13 +5,21 @@ int main()
     scanf("%c", &a);
     if (a >= '0' && a <= '9')
     {
-        printf("It's Not A Alphabet Input A Alphabet.");
+        printf("Input Number is: %c, And Output Number Is: %c", a, a);
     }
     else
     {
         if (a >= 'a' && a <= 'z')
         {
-            Printf("%c", a+32);
+            printf("Input Lowercase: %c, Output Uppercase: %c", a, a - 32);
+        }
+        else if (a >= 'A' && a <= 'Z')
+        {
+            printf("Input Uppercase: %c, Output Lowercase: %c", a, a + 32);
+        }
+        else
+        {
+            printf("Input Value Is Not A Number Or Letter");
         }
     }
     return 0;
