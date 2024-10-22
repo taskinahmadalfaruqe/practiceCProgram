23079c199bd65986b7e60f3638ed6f5ac086cd3b
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2024-10-22 20:58:47 +0800

    Resolve Previous Problem

--- Module-02.5/problem01.c
+++ Module-02.5/problem01.c
@@ -1,12 +1,19 @@
 #include <stdio.h>
 int main()
 {
-    int a,b;
-    scanf("%d%d",&a,&b);
-    if(a>b){
-        printf("A IS Geter Then B: %d:",a);
-    }else{
-        printf("B IS Geter Then A: %d:",b);
+    int a, b;
+    scanf("%d%d", &a, &b);
+    if (a > b)
+    {
+        printf("A IS Geter Then B: %d:", a);
+    }
+    else if (a == b)
+    {
+        printf("A & B Both Are Equal.");
+    }
+    else
+    {
+        printf("B IS Geter Then A: %d:", b);
     }
     return 0;
 }
