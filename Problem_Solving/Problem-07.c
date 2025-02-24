c0a4a42d399af7cac5e1d4184f1ac53477c92a08
taskinahmadalfaruqe <taskinahmed180@gmail.com>
2025-02-25 03:06:44 +0800

    add Zero One Again Problem.

---
+++ Problem_Solving/Problem-07.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+int main()
+{
+    int a, b;
+    scanf("%d", &a);
+    int ar[a];
+    for (int i = 0; i < a; i++)
+    {
+        scanf("%d", &ar[i]);
+    }
+
+    scanf("%d", &b);
+
+    if (b >= 0 && b <= a)
+    {
+        if (ar[b - 1] == 0)
+        {
+            ar[b - 1] = 1;
+        }
+        else
+        {
+            ar[b - 1] = 0;
+        }
+    }
+    for (int i = 0; i < a; i++)
+    {
+        printf("%d ", ar[i]);
+    }
+    return 0;
+}
